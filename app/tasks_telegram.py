d8fafaec9c1673cb6342cc4db55e588ba3563a98
omarfadl <omar.fadlallah1@gmail.com>
2025-05-11 01:40:57 +0100

    adding-telegram-test-code

--- app/tasks_telegram.py
+++ app/tasks_telegram.py
@@ -1,7 +1,32 @@
 from app.celery_worker import celery
+import os
 import requests
 
+TELEGRAM_GATEWAY_TOKEN = os.getenv("TELEGRAM_GATEWAY_TOKEN")
+END_POINT_SEND_VERIFICATION_MESSAGE="sendVerificationMessage"
+
 @celery.task(name="app.tasks_telegram.send_via_telegram")
-def send_via_telegram(to, message, account_id):
-    print(f"[TG] Sending to {to} | {account_id}: {message}")
-    # Replace with actual Telegram Bot API logic
+def send_via_telegram(to: str, code: str, account_id: str):
+    print(f"[Telegram] Sending OTP to {to} | {account_id}: {code}")
+
+    url = "https://gatewayapi.telegram.org/sendVerificationMessage"
+    headers = {
+        "Authorization": f"Bearer {TELEGRAM_GATEWAY_TOKEN}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "phone_number": to,
+        "code": code,
+        "ttl": 300,
+        "payload": account_id
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data,timeout=10)
+        response.raise_for_status()
+        result = response.json()
+        print(f"[Telegram] API response: {result}")
+        return result
+    except requests.RequestException as e:
+        print(f"[Telegram] Failed to send OTP: {e}")
+        return {"status": "error", "reason": str(e)}
